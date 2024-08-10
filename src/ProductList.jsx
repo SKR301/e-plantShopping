3925f0c9603e454cf06f500478c16f08ea67aab0
SKR301 <skrint301@gmail.com>
2024-08-10 07:05:36 -0400

    product page updated

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -233,6 +233,8 @@ function ProductList() {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
+        display: 'flex',
+        justifyContent: 'flex-end',
     }
    const handleCartClick = (e) => {
         e.preventDefault();
@@ -250,10 +252,10 @@ function ProductList() {
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
         setAddedToCart((prevState) => ({
-           ...prevState,
-           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-         }));
-      };
+            ...prevState,
+            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+        }));
+    };
 
     return (
         <div>
@@ -271,15 +273,22 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
+
             {!showCart? (
                 <div className="product-grid">
                     {plantsArray.map((category, index) => (
                         <div key={index}>
                             <br />
-                            <center><h1><div>{category.category}</div></h1></center>
+                            <center><h1><u>{category.category}</u></h1></center>
                             <br />
                             <div className="product-list">
                                 {category.plants.map((plant, plantIndex) => (
@@ -289,6 +298,7 @@ function ProductList() {
                                     <h2 className="product-cost" style={{color: 'red'}}>{plant.cost}</h2>
                                     <div className="product-description">{plant.description}</div>
                                     {/*Similarly like the above plant.name show other details like description and cost*/}
+                                    <br />
                                     <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                 </div>
                                 ))}
@@ -296,7 +306,7 @@ function ProductList() {
                         </div>
                     ))}
                 </div>
-            ) :  (
+            ) : (
                 <CartItem onContinueShopping={handleContinueShopping}/>
             )}
         </div>
